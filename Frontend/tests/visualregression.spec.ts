d580156321a19fba1fdc1c14b40cea1d36c5c96b
Lukas Yu <lukas-yu@hotmail.com>
2022-12-13 19:28:33 +0100

    Change screenshot type to fullpage

--- Frontend/tests/visualregression.spec.ts
+++ Frontend/tests/visualregression.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test("Expect the page to look the same", async ({page}) => {
     await page.goto('http://localhost:4200');
-    await expect(page).toHaveScreenshot("homepage.png");
+    await expect(page).toHaveScreenshot("homepage.png", { fullPage: true });
 });
