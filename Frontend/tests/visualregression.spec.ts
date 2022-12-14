90fe62001aadcc00366a4d2efcc8f6d827a57e02
Lukas Yu <lukas-yu@hotmail.com>
2022-12-14 10:13:05 +0100

    Add mask and higher pixel threshold

--- Frontend/tests/visualregression.spec.ts
+++ Frontend/tests/visualregression.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test("Expect the page to look the same", async ({page}) => {
+    let maskLocator = page.locator('#total-balance')
+
     await page.goto('http://localhost:4200');
-    await expect(page).toHaveScreenshot("homepage.png", { fullPage: true });
+    await expect(page).toHaveScreenshot("homepage.png", { fullPage: true, maxDiffPixels: 100, mask: [ maskLocator ] });
 });
