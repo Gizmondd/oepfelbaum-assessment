f9e9a3ea9664af2692365c783a56c89d7301b08c
Lukas Yu <lukas-yu@hotmail.com>
2022-12-15 15:36:50 +0100

    Add mock stub for pilot test

--- Frontend/cypress/e2e/pilot.cy.ts
+++ Frontend/cypress/e2e/pilot.cy.ts
@@ -1,7 +1,10 @@
+const mockData = '[{"id":"5289a241-4a94-4e7a-942e-e6be8cad9d56","name":"Mock Saving Account","type":"Savings","balance":125662.22},{"id":"747db1cf-581d-48e1-be94-99c8e212c356","name":"Mock Private Account","type":"CurrentAccount","balance":-1420.14}]'
+
 describe('First steps', () => {
   it('Visits the site', () => {
-    cy.visit('localhost:4200')
 
-    cy.get('.list-group-item').first().click();
-  })
-})
+    cy.intercept('http://localhost:4000/accounts', mockData );
+    cy.visit('localhost:4200');
+
+  });
+});
